375a82cb8184e4e2458f595b573f9da1bd1f66e0
TryEye <tb1093612@gmail.com>
2026-08-07 15:46:22 +0530

    Add Support, Queries & Bug Reports section to footer with Framer Motion cards and contact Modal

---
+++ src/config/contacts.ts
@@ -0,0 +1,24 @@
+export interface Contact {
+  name: string;
+  role: string;
+  linkedin: string;
+  email: string;
+  phone: string;
+}
+
+export const CONTACTS: Contact[] = [
+  {
+    name: 'Tanishk Bansal',
+    role: 'Project Maintainer',
+    linkedin: 'https://www.linkedin.com/in/tanishk-bansal-',
+    email: 'tanishk.bansal2025@glbajajgroup.org',
+    phone: '+91 8534998412',
+  },
+  {
+    name: 'Ayush Sharma',
+    role: 'Project Maintainer',
+    linkedin: 'https://www.linkedin.com/in/ayushh-sharmaa/',
+    email: 'ayush.sharma2025@glbajajgroup.org',
+    phone: '+91 89239995135',
+  },
+];
